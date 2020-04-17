6700812900eb662a44ff1652d22c1006babf5233
Henrik Forsberg <henrikforsberg@pop-os.localdomain>
2020-04-17 11:26:13 +0200

    Now it works

--- Domain.fs
+++ Domain.fs
@@ -1,43 +1,47 @@
-module Domain 
+module Domain
 
 open System.IO
 open FSharp.Data
 
-module FileProcessor = 
-    
-    let getFile (fileName: string) = 
-        CsvFile.Load(fileName, hasHeaders = true) 
-        |> fun x -> x.Rows 
-        |> Seq.map  (fun x -> (x.["escalation_policy_name"], x.["created_on"]))
+module FileProcessor =
+
+    let getFile (fileName: string) =
+        CsvFile.Load(fileName, hasHeaders = true)
+        |> fun x -> x.Rows
+        |> Seq.map (fun x -> (x.["escalation_policy_name"], x.["created_on"]))
         |> Seq.toArray
 
     let ifRaven (word: string) =
         if word = "Team Raven" then 1 else 0
 
-    let getPointByLine (team: string, date:string) =
-        team |> ifRaven, 
-        date |> System.DateTime.Parse |> System.Globalization.ISOWeek.GetWeekOfYear
+    let getPointByLine (team: string, date: string) =
+        date
+        |> System.DateTime.Parse
+        |> System.Globalization.ISOWeek.GetWeekOfYear, team |> ifRaven
 
-    let getTeamRavenErrors (values: (string * string) []) =    
+    let groupAndFlatten (array: (int * int) []) =
+        array
+        |> Array.groupBy (fun (week, _) -> week)
+        |> Array.map (fun (week: int, values) -> week, values |> Array.sumBy (fun (_, count) -> count))
+
+    let getTeamRavenErrors (values: (string * string) []) =
         values
-            |> Array.map getPointByLine
-            |> Array.groupBy (fun (_, week) -> week)
-            |> Array.map (fun (week: int, values) -> week, values |> Array.sumBy (fun (count, _) -> count) )
+        |> Array.map getPointByLine
+        |> groupAndFlatten
 
-    let formatOutput (values: (int * int) []) = 
-        values 
+    let formatOutput (values: (int * int) []) =
+        values
         |> Array.map (fun (week, count) -> week.ToString() + ", " + count.ToString())
-        |> Array.append [|"week, amount of errors."|]
-    
-    let processFiles(output:(string [] -> unit), path: string) =
+        |> Array.append [| "week, amount of errors." |]
+
+    let processFiles (output: string [] -> unit, path: string) =
         path
         |> Directory.GetFiles
-        |> Array.collect
-            (getFile
-             >> getTeamRavenErrors 
-             >> formatOutput)
+        |> Array.collect (getFile >> getTeamRavenErrors)
+        |> groupAndFlatten
+        |> formatOutput
         |> output
 
 module Outputer =
-    let printToDisc = fun (x: string [])-> File.WriteAllLines("output.csv", x)
+    let printToDisc = fun (x: string []) -> File.WriteAllLines("output.csv", x)
     let printToConsole = (fun x -> printfn "%A," x)
